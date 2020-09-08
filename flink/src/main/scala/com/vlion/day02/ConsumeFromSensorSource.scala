75c4fd375f4491abd5cc479478d13b0085f3ad0f
malichun_glb <malichun0926@qq.com>
2020-09-09 00:37:52 +0800

    添加数据源

---
+++ flink/src/main/scala/com/vlion/day02/ConsumeFromSensorSource.scala
@@ -0,0 +1,23 @@
+package com.vlion.day02
+
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import org.apache.flink.api.scala._
+
+/**
+ * Created by John.Ma on 2020/9/9 0009 0:33
+ */
+object ConsumeFromSensorSource {
+    def main(args: Array[String]): Unit = {
+
+        val env = StreamExecutionEnvironment.getExecutionEnvironment
+
+        env.setParallelism(1)
+
+        //调用addSource方法
+        val stream = env.addSource(new SensorSource)
+
+        stream.print()
+
+        env.execute()
+    }
+}
---
+++ flink/src/main/scala/com/vlion/day02/SensorReading.scala
@@ -0,0 +1,10 @@
+package com.vlion.day02
+
+/**
+ * Created by John.Ma on 2020/9/9 0009 0:14
+ * 传感器读书的数据结构
+ */
+case class SensorReading(id: String, // 传感器id
+                         timestamp: Long, //时间戳
+                         temperature: Double //温度
+                        )
