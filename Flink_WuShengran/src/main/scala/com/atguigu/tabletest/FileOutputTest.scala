6997581e5a8a7d6e1b88eeff430aede56da1ce2a
malichun_glb <malichun0926@qq.com>
2020-09-17 22:46:47 +0800

    FileOutputTest

--- Flink_WuShengran/src/main/scala/com/atguigu/tabletest/FileOutputTest.scala
+++ Flink_WuShengran/src/main/scala/com/atguigu/tabletest/FileOutputTest.scala
@@ -20,55 +20,42 @@ object FileOutputTest {
         env.setParallelism(1)
         val tableEnv = StreamTableEnvironment.create(env)
 
-
         //2.连接外部系统,读取数据,注册表
-        val filePath = getClass.getResource("/sensor.txt").getPath
-
-        tableEnv.connect(new FileSystem().path(filePath))
+        tableEnv.connect(new FileSystem().path("D:\\fileImportant\\Learn_projects\\learn\\Flink_WuShengran\\src\\main\\resources\\sensor.txt"))
             .withFormat(new Csv())
             .withSchema(new Schema()
                 .field("id", DataTypes.STRING())
                 .field("timestamp", DataTypes.BIGINT())
                 .field("temp", DataTypes.DOUBLE())
-                //              .field("pt",DataTypes.TIMESTAMP(3)).proctime()
             ).createTemporaryTable("inputTable")
 
         //3.转换操作
         val sensorTable = tableEnv.from("inputTable")
-
         //3.1简单转换
         val resultTable = sensorTable
             .select('id, 'temp)
             .filter('id === "sensor_1")
 
-        //3.2复杂转换
+        // 3.2聚合转换
         val aggTable = sensorTable
             .groupBy('id) //基于id分组
-            .select('id, 'id.count as 'count )
+            .select('id, 'id.count as 'count)
 
-        //4.输出到文件
-        //注册输出表
-        val outputPath = "E:\\gitdir\\learn_projects\\myLearn\\Flink_WuShengran\\src\\main\\resources\\output.txt"
 
-        tableEnv.connect(new FileSystem().path(outputPath))
-            .withFormat(new Csv())
+        // 4.输出到文件
+        tableEnv.connect(new FileSystem().path("D:\\fileImportant\\Learn_projects\\learn\\Flink_WuShengran\\src\\main\\resources\\out.txt"))
+            .withFormat(new Csv)
             .withSchema(new Schema()
-                .field("id", DataTypes.STRING())
-                .field("temperature", DataTypes.DOUBLE())
-//                .field("cnt", DataTypes.BIGINT())
-            )
-            .createTemporaryTable("outputTable")
+                .field("id1", DataTypes.STRING())
+                .field("temperature1", DataTypes.DOUBLE())  //输出表的字段名字可以和insert表的名字的字段不一样
+            ).createTemporaryTable("outputTable")
 
-        //输出到文件,直接会生成文件,不是output.txt文件夹!
+        //        resultTable.toAppendStream[(String,Double)].print("result")
+        //        aggTable.toRetractStream[(String,Long)].print("agg result") //返回带一个boolean
+
         resultTable.insertInto("outputTable")
-
-        resultTable.toAppendStream[(String, Double)].print("result")
-
-        //group后要用toRetractStream方法
-        aggTable.toRetractStream[Row].print("agg")
-
-        env.execute()
-
+//        aggTable.insertInto("")  //不行,聚合之后的函数不行,更改操作不行 AppendStreamTableSink
+        env.execute("file output test")
 
     }
 }
