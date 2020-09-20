772f42b4f5944f38c17787e0eb1957f6ea7d0604
malichun_glb <malichun0926@qq.com>
2020-09-20 11:52:47 +0800

    bab

--- Flink_WuShengran/src/main/scala/com/atguigu/tabletest/TimeAndWindowTest.scala
+++ Flink_WuShengran/src/main/scala/com/atguigu/tabletest/TimeAndWindowTest.scala
@@ -5,7 +5,7 @@ import org.apache.flink.streaming.api.TimeCharacteristic
 import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor
 import org.apache.flink.streaming.api.scala._
 import org.apache.flink.streaming.api.windowing.time.Time
-import org.apache.flink.table.api.{EnvironmentSettings, Over, Table, Tumble}
+import org.apache.flink.table.api.{EnvironmentSettings, Over, Tumble}
 import org.apache.flink.table.api.scala._
 import org.apache.flink.types.Row
 
@@ -19,93 +19,107 @@ object TimeAndWindowTest {
     def main(args: Array[String]): Unit = {
         val env = StreamExecutionEnvironment.getExecutionEnvironment
         env.setParallelism(1)
-        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)  //设置事件时间
 
-        val setting = EnvironmentSettings.newInstance()
+        //设置时间特性
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
+
+        val settings = EnvironmentSettings.newInstance()
             .useBlinkPlanner()
             .inStreamingMode()
             .build()
-
-        val tableEnv = StreamTableEnvironment.create(env,setting)
+        //表环境
+        val tableEnv = StreamTableEnvironment.create(env, settings)
 
         //读取数据
-        val inputPath = "E:\\gitdir\\learn_projects\\myLearn\\Flink_WuShengran\\src\\main\\resources\\sensor.txt"
-
+        val inputPath = "D:\\fileImportant\\Learn_projects\\learn\\Flink_WuShengran\\src\\main\\resources\\sensor.txt"
         val inputStream = env.readTextFile(inputPath)
 
-        //先转换成样例类类型
+        //先转换成样例类类型(简单转换操作)
         val dataStream = inputStream
             .map(data => {
                 val arr = data.split(",")
-                SensorReading(arr(0),arr(1).toLong,arr(2).toDouble)
+                SensorReading(arr(0), arr(1).toLong, arr(2).toDouble)
             })
-            //确定时间watermark
-            .assignTimestampsAndWatermarks(new BoundedOutOfOrdernessTimestampExtractor[SensorReading](Time.seconds(1)) {
-                override def extractTimestamp(element: SensorReading): Long = {
-                    element.timestamp * 1000L
-                }
+            .assignTimestampsAndWatermarks(new BoundedOutOfOrdernessTimestampExtractor[SensorReading](Time.milliseconds(1000)) {
+                override def extractTimestamp(element: SensorReading): Long = element.timestamp * 1000L
             })
 
-        val sensorTable: Table = tableEnv.fromDataStream(dataStream,'id,'temperature,'timestamp.rowtime as 'ts)
+        //        val sensorTable = tableEnv.fromDataStream(dataStream,'id,'temperature,'timestamp,'pt.rowtime)  //'pt.rowtime 提取的时间戳
+        val sensorTable = tableEnv.fromDataStream(dataStream, 'id, 'temperature, 'timestamp.rowtime as 'ts)
 
-//group By window ///////////////
-        //1.Group Window
+/** 窗口测试 */
+
+/** Group Window */
+        //窗口测试
+        //1.Group Window   //需求每10s钟的窗口,根据id分组,每个id有多少值进来,滚动窗口
         //1.1 table api
-        val resultTable: Table = sensorTable
-            .window(Tumble over 10.seconds on 'ts as 'tw) //每10秒统计一次,滚动时间窗口
-            .groupBy('id,'tw)
-            .select('id, 'id.count, 'temperature.avg, 'tw.end)
+        val resultTable = sensorTable
+            .window(Tumble over 10.seconds on 'ts as 'tw)
+            .groupBy('id, 'tw)
+            .select('id, 'id.count, 'temperature.avg, 'tw.end) //'tw.end 当前窗口的关窗时间
 
-        //1.2 sql
-        tableEnv.createTemporaryView("sensor",sensorTable)
+        //1.2.sql
+        tableEnv.createTemporaryView("sensor", sensorTable)
         val resultSqlTable = tableEnv.sqlQuery(
             """
+              |
               |select
-              | id,
-              | count(id),
-              | avg(temperature),
-              | tumble_end(ts,interval '10' second)
-              |from sensor
+              |   id,
+              |   count(id),
+              |   avg(temperature),
+              |   tumble_end(ts,interval '10' second)  -- 当前窗口的关窗时间
+              |from
+              |   sensor
               |group by
-              | id,
-              | tumble(ts,interval '10' second)
+              |   id,
+              |   tumble(ts, interval '10' second)
               |
               |""".stripMargin)
 
+        //转换成流打印输出
+        //        resultTable.toAppendStream[Row].print("result")  //写入就不会更改了
+        //        resultSqlTable.toRetractStream[Row].print("sql result")
 
-////over window////////////
-
-        //2.Over window: 统计每个sensor每条数据,与之前两行数据的平均温度
+/** 2.Over Window */
+        //2. Over Window:统计每个sensor每条数据,与之前两行数据的平均温度(按照事件时间排序)
         //2.1 table api
         val overResultTable = sensorTable
             .window(Over partitionBy 'id orderBy 'ts preceding 2.rows as 'ow)
-            .select('id, 'ts, 'id.count over 'ow,'temperature.avg over 'ow)
+                .select('id, 'ts, 'id.count over 'ow, 'temperature.avg over 'ow)
 
-        //2.2sql
-        val overResultSqlTable = tableEnv.sqlQuery(
-            """
-              |select
-              |     id,
-              |     ts,
-              |     count(id) over ow,
-              |     avg(temperature) over ow
-              |from
-              |     sensor
-              |window ow as (
-              |     partition by id
-              |     order by ts
-              |     rows between 2 preceding and current row
-              |)
-              |""".stripMargin
-        )
+        //2.2 sql
+        val overResultSqlTable = tableEnv
+                .sqlQuery(
+                    """
+                      |select
+                      |
+                      | id,
+                      | ts,
+                      | count(id) over ow,
+                      | avg(temperature) over ow
+                      |from
+                      | sensor
+                      |window ow as (
+                      | partition by id
+                      | order by ts
+                      | rows between 2 preceding and current row
+                      |
+                      |)
+                      |
+                      |""".stripMargin)
+        overResultTable.toAppendStream[Row].print("result")  //写入就不会更改了
+        overResultSqlTable.toRetractStream[Row].print("sql result")
 
-        //转换成流输出
-        overResultTable.toAppendStream[Row].print("result")
-        overResultSqlTable.toRetractStream[Row].print("sql")
 
-//        sensorTable.printSchema()
-//        sensorTable.toAppendStream[Row].print()
+        //        sensorTable.printSchema()
+        //root
+        // |-- id: STRING
+        // |-- temperature: DOUBLE
+        // |-- timestamp: BIGINT
+        // |-- pt: TIMESTAMP(3) *PROCTIME*
+
+        //        sensorTable.toAppendStream[Row].print()
+
         env.execute("time and window test")
-
     }
 }
--- Flink_WuShengran/src/main/scala/com/atguigu/tabletest/udftest/ScalarFunctionTest.scala
+++ Flink_WuShengran/src/main/scala/com/atguigu/tabletest/udftest/ScalarFunctionTest.scala
@@ -24,56 +24,57 @@ object ScalarFunctionTest {
         env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
 
         val settings = EnvironmentSettings.newInstance()
-            .useBlinkPlanner()
             .inStreamingMode()
+            .useBlinkPlanner()
             .build()
 
         val tableEnv = StreamTableEnvironment.create(env,settings)
 
-        //读取数据
-        val inputPath = "E:\\gitdir\\learn_projects\\myLearn\\Flink_WuShengran\\src\\main\\resources\\sensor.txt"
-        val inputStream = env.readTextFile(inputPath)
-
-        //先转换成样例类类型
-        val dataStream = inputStream
-            .map(data => {
+        val dataStream = env
+            .readTextFile("D:\\fileImportant\\Learn_projects\\learn\\Flink_WuShengran\\src\\main\\resources\\sensor.txt")
+            .map((data:String) =>{
                 val arr = data.split(",")
-                SensorReading(arr(0), arr(1).toLong, arr(2).toDouble)
+                SensorReading(arr(0),arr(1).toLong,arr(2).toDouble)
             })
-            .assignTimestampsAndWatermarks(new BoundedOutOfOrdernessTimestampExtractor[SensorReading](Time.seconds(1)) {
+            .assignTimestampsAndWatermarks(new BoundedOutOfOrdernessTimestampExtractor[SensorReading](Time.seconds(1)){
                 override def extractTimestamp(element: SensorReading): Long = {
-                    element.timestamp* 1000L
+                    element.timestamp * 1000L
                 }
             })
 
         val sensorTable = tableEnv.fromDataStream(dataStream,'id,'temperature,'timestamp.rowtime as 'ts)
 
-
         //调用自定义hash函数,对id进行hash运算
-        //1.table api
-        //首先new 一个UDF的实例
+        //1. table api
+        //首先new 一个udf的实例
         val hashCode = new HashCode(23)
-
-
         val resultTable = sensorTable
-            .select('id,'ts, hashCode('id))
+                .select('id,'ts,hashCode('id)) // ?
 
-        //2.sql
-        //需要在环境中注册UDF
+        // 2. sql
+        //需要在表环境中注册udf
         tableEnv.createTemporaryView("sensor",sensorTable)
         tableEnv.registerFunction("hashCode",hashCode)
-        val resultSqlTable = tableEnv.sqlQuery("select id, ts, hashCode(id) from sensor")
+        val resultSqlTable = tableEnv.sqlQuery(
+            """
+              | select
+              |     id,ts,hashCode(id)
+              | from
+              |     sensor
+              |
+              |""".stripMargin)
 
         resultTable.toAppendStream[Row].print("result")
         resultSqlTable.toAppendStream[Row].print("sql")
 
         env.execute("scalar function test")
+
     }
 }
 
 // 自定义标量函数
-class HashCode(factor:Int) extends ScalarFunction{
-    def eval(s:String):Int = {
+class HashCode( factor:Int) extends ScalarFunction{
+    def eval(s:String):Int={
         s.hashCode * factor - 10000
     }
 }
