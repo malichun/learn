23d1142298543d61887cc399c05354a09507e711
malichun_glb <malichun0926@qq.com>
2021-04-28 01:13:59 +0800

    phantom reference

--- jvm/src/main/java/com/atguigu/chapter15_GC_CONCEPT/java1/SoftReferenceTest.java
+++ jvm/src/main/java/com/atguigu/chapter15_GC_CONCEPT/java1/SoftReferenceTest.java
@@ -25,31 +25,29 @@ public class SoftReferenceTest {
     }
 
     public static void main(String[] args) {
-        //创建对象，建立软引用
-//        SoftReference<User> userSoftRef = new SoftReference<User>(new User(1, "songhk"));
-        //上面的一行代码，等价于如下的三行代码
-        User u1 = new User(1,"songhk");
-        SoftReference<User> userSoftRef = new SoftReference<User>(u1);
-        u1 = null;//取消强引用
+        // 创建对象,建立软引用
+//        SoftReference<User> userSoftRef = new SoftReference<>(new User(1,"sonhk"));
+        //上面一行代码,等价于如下的三行代码
+        User u1 = new User(1,"shkstart");
+        SoftReference<User> userSoftReference = new SoftReference<>(u1);
+        u1 = null; // 取消强引用
 
-
-        //从软引用中重新获得强引用对象
-        System.out.println(userSoftRef.get());
+        // 从软引用中重新获取强引用对象
+        System.out.println(userSoftReference.get());
 
         System.gc();
         System.out.println("After GC:");
-//        //垃圾回收之后获得软引用中的对象
-        System.out.println(userSoftRef.get());//由于堆空间内存足够，所有不会回收软引用的可达对象。
-//
+        //垃圾回收之后获得软引用中的对象
+        System.out.println(userSoftReference.get()); //由于堆空间内存足够,所以不会回收软引用的可达对象。
         try {
-            //让系统认为内存资源紧张、不够
-//            byte[] b = new byte[1024 * 1024 * 7];
-            byte[] b = new byte[1024 * 7168 - 635 * 1024];
-        } catch (Throwable e) {
+            byte[] b = new byte[1024 * 1024 * 7];
+        }catch (Throwable e){
             e.printStackTrace();
-        } finally {
-            //再次从软引用中获取数据
-            System.out.println(userSoftRef.get());//在报OOM之前，垃圾回收器会回收软引用的可达对象。
+        }finally {
+            // 再次从软引用中获取数据
+            System.out.println(userSoftReference.get());//在报OOM之前，垃圾回收器会回收软引用的可达对象。
         }
+
     }
+
 }
